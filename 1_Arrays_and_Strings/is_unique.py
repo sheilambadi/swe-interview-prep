6d44157391f67f0b70cbf60edeba62a126fc87eb
sheilambadi <mbadisheila@gmail.com>
2021-06-09 10:42:08 +0200

    Add unittest to code

--- 1_Arrays_and_Strings/is_unique.py
+++ 1_Arrays_and_Strings/is_unique.py
@@ -1,8 +1,9 @@
 """
-Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
-cannot use additional data structures?
+    Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+    cannot use additional data structures?
 """
 
+import unittest
 from collections import Counter
 
 
@@ -12,12 +13,9 @@ def is_unique_dict(word):
 
     return all(v == 1 for v in vals)
 
-print(is_unique_dict("Hello"))
-print(is_unique_dict("Helo"))
-
 def is_unique(word):
     """
-    Implementation without an additional data structure
+        Implementation without an additional data structure
     """
 
     # store previous letter in sorted list
@@ -33,5 +31,30 @@ def is_unique(word):
 
     return True
 
-print(is_unique("Hello"))
-print(is_unique("Helo"))
+class Test(unittest.TestCase):
+    """
+        Adapted from: https://github.com/careercup/CtCI-6th-Edition-Python/blob/e6bc732588601d0a98e5b1bc44d83644b910978d/Chapter1/1_Is%20Unique/IsUnique.py
+    """
+    dataT = [('abcd'), ('s4fad'), ('')]
+    dataF = [('23ds2'), ('hb 627jh=j ()')]
+
+    def test_is_unique_dict(self):
+        for test_string in self.dataT:
+            actual = is_unique_dict(test_string)
+            self.assertTrue(actual)
+
+        for test_string in self.dataF:
+            actual = is_unique_dict(test_string)
+            self.assertFalse(actual)
+
+    def test_is_unique(self):
+        for test_string in self.dataT:
+            actual = is_unique(test_string)
+            self.assertTrue(actual)
+
+        for test_string in self.dataF:
+            actual = is_unique(test_string)
+            self.assertFalse(actual)
+
+if __name__ == "__main__":
+    unittest.main()
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-Topics Covered
+# Topics Covered
 
 1. [Arrays and Strings](./1_Arrays_and_Strings)
+    - [Is Unique](./1_Arrays_and_Strings/is_unique.py)
